cb6e58543d2a270f6d0afa4d6802c821c1b5ceb4
Wesley Pedroso <wesley.soares.pedroso@hotmail.com>
2020-02-25 16:43:04 -0300

    Hello SpringBoot and REST Udemy

---
+++ RestWithSpringBootUdemy/src/main/java/br/com/erudio/GreetingController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.erudio;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author wesle
+ */
+@RestController
+public class GreetingController {
+   
+    private static final String template = "Hello, %s! ";
+    
+    private final AtomicLong counter = new AtomicLong();
+    
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        
+        
+    }
+        
+}
---
+++ RestWithSpringBootUdemy/src/main/java/br/com/erudio/Startup.java
@@ -0,0 +1,25 @@
+package br.com.erudio;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author wesle
+ */
+
+
+@SpringBootApplication
+public class Startup {
+    
+    public static void main(String[] args) {
+      SpringApplication.run(Startup.class, args);    
+    }
+}
